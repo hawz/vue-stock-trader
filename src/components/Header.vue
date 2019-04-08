add4d19fa47025bcd1040b0f3db11fdf775d9df3
Alberto Masia <alberto.masia@gmail.com>
2019-04-08 17:55:32 +0200

    Define and bind loadData and saveData in Header. Fix dropdown.

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,14 +10,20 @@
           <router-link to="/portfolio" activeClass="active" tag="li">
             <a>Portfolio</a>
           </router-link>
-          <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
+          <router-link to="/stocks" activeClass="active" tag="li">
+            <a>Stocks</a>
+          </router-link>
         </ul>
         <strong class="navbar-text navbar-right">Funds: {{ funds | currency }}</strong>
         <ul class="nav navbar-nav navbar-right">
           <li>
             <a href="#" @click="endDay">End Day</a>
           </li>
-          <li class="dropdown">
+          <li
+            class="dropdown"
+            :class="{ open: isDropdownOpen }"
+            @click="isDropdownOpen = !isDropdownOpen"
+          >
             <a
               href="#"
               class="dropdown-toggle"
@@ -31,10 +37,10 @@
             </a>
             <ul class="dropdown-menu">
               <li>
-                <a href="#">Save</a>
+                <a href="#" @click="saveData">Save</a>
               </li>
               <li>
-                <a href="#">Load</a>
+                <a href="#" @click="loadData">Load</a>
               </li>
             </ul>
           </li>
@@ -46,19 +52,40 @@
   </nav>
 </template>
 <script>
-import { mapActions } from 'vuex';
+import { mapActions } from "vuex";
 
 export default {
+  data() {
+    return {
+      isDropdownOpen: false
+    };
+  },
   computed: {
     funds() {
       return this.$store.getters.funds;
     }
   },
   methods: {
-    ...mapActions(['randomizeStocks']),
+    ...mapActions({
+      randomizeStocks: 'randomizeStocks',
+      fetchData: 'loadData'
+    }),
     endDay() {
       this.randomizeStocks();
+    },
+    saveData() {
+      const data = {
+        funds: this.$store.getters.funds,
+        stockPortfolio: this.$store.getters.stockPortfolio,
+        stocks: this.$store.getters.stocks
+      };
+
+      this.$http.put('data.json', data);
+    },
+    loadData() {
+      // here we actually change the app state, so we gotta go through the store
+      this.fetchData();
     }
   }
-}
+};
 </script>
